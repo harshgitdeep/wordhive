4f25b5f5b0dee58cdfda57908ba33c37e529fc8f
harshgitdeep <harshzone3@gmail.com>
2026-05-21 20:20:13 +0530

    fix footer section text alignment

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
-import "./App.css"; 
+import "./App.css";
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="container mx-auto">
-        <div className="flex justify-center items-center">
-          <p className="text-gray-600 text-sm">&copy; 2026 WordHive. All rights reserved. Made by Harshdeep Singh</p>
+        <div className="flex flex-col justify-center items-center space-y-2">
+          <p className="text-gray-600 text-sm">
+            &copy; 2026 WordHive. All rights reserved. Made by Harshdeep Singh
+          </p>
           <FooterLinks />
         </div>
       </div>
@@ -29,11 +31,23 @@ function FooterLinks() {
         <Link to="/privacy">Privacy Policy</Link>
       </li>
       <li>
-      <a href="https://github.com/harshgitdeep/wordhive" target="_blank" rel="noopener noreferrer">Source Code</a>
-    </li>
+        <a
+          href="https://github.com/harshgitdeep/wordhive"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </li>
       <li>
-      <a href="https://www.linkedin.com/in/harshdeepsingh-/" target="_blank" rel="noopener noreferrer">Linkedin</a>
-    </li>
+        <a
+          href="https://www.linkedin.com/in/harshdeepsingh-/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+      </li>
     </ul>
   );
 }
